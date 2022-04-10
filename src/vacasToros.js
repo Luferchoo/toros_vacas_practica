16b8816ddd41706757fddc720e4e02e88cc62412
Luis Fernando Gil Gareca <luisfernandogilgareca@gmail.com>
2022-04-10 17:44:46 -0400

    refactor: se extrajo un m[etodo de esception para que pueda trabajar con 2 codigos y compararlos

--- src/vacasToros.js
+++ src/vacasToros.js
@@ -1,7 +1,15 @@
 import Secretista from "./secretista";
 import Adivinador from "./adivinador";
+import Exception from "./exception";
+
 function torosVacas(codigoSecreto, adivinanza){
-    return "Intento no válido, cantidad de dígitos errónea, por favor ingrese una cantidad válida";
+    let verificador = new Exception("OK");
+    verificador.verificador(codigoSecreto.length != adivinanza.length,"cantidad de dígitos errónea, por favor ingrese una cantidad válida");
+    let numMostrado = "Intento no válido, " + verificador.mensaje;
+    if(verificador.error === false){
+        numMostrado = codigoSecreto;
+    }
+    return numMostrado;
 }
 
 export default torosVacas;
