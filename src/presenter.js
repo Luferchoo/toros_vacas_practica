cbf57afe7dd8aacc5d8726fac7696eeb6c3b6df3
Luis Fernando Gil Gareca <luisfernandogilgareca@gmail.com>
2022-04-12 11:21:26 -0400

    preparacion para examen de 2do parcial

--- src/presenter.js
+++ src/presenter.js
@@ -1,17 +1,20 @@
 import { torosVacas } from "./vacasToros";
 
-const secret = document.querySelector("#codigo-secreto");
-const intento = document.querySelector("#intento");
-const form = document.querySelector("#juego");
-const div = document.querySelector("#resultado-div");
+    const secret = document.querySelector("#codigo-secreto");
+    const intento = document.querySelector("#intento");
+    const form = document.querySelector("#juego");
+    const div = document.querySelector("#resultado-div");
+    
+    form.addEventListener("submit", (event) => {
+      event.preventDefault();
+      var index = 10;
+      const secretNumber = String(secret.value);
+      const intentoNumber = String(intento.value);
+      const intentos = torosVacas(secretNumber,intentoNumber,index);
+      index -= 1;
+      div.innerHTML = "<p>" + intentos[1] + "</p>";
+    });
+ 
 
-form.addEventListener("submit", (event) => {
-  event.preventDefault();
-  const contador = 10;
-  const secretNumber = String(secret.value);
-  const intentoNumber = String(intento.value);
-  const retroalimentacion="";
-  const intentos = torosVacas(secretNumber,intentoNumber,contador);
-  div.innerHTML = "<p>" + intentos[1] + "</p>";
-});
+
 
